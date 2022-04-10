4f95b6131c670aec59e8264d05a1ea6e4b7a1a8d
xiw020-Sam <xiw020@ucsd.edu>
2022-04-10 02:29:36 -0700

    Adding App Framework v1

---
+++ CovidSuppliesTrade/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Sam Wang on 3/3/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    @IBOutlet weak var sellOrBuyLabel: UILabel!
+    @IBOutlet weak var urgencyLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
